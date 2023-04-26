3c1c840a369513538e64884a99f01cef3ed2e42a
AlejandroPolskoy <mitchel_nk@yahoo.es>
2023-04-26 14:45:47 +0200

    trying to fix vercel again and again and over again

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ server.use((req, res , next) => {
     res.header('Access-Control-Allow-Method', 'POST, GET, DELETE, PUT, PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Origin', '*'); 
     next();
 })
 
